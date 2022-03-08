99c5d26205b834aa5f36bb932ef1fb13987d6148
Vijay Bhosale <vijay.bhosale@neosoftmail.com>
2022-03-08 14:30:04 +0530

    first commit

---
+++ app/src/main/java/com/xendit/wordle/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.xendit.wordle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.xendit.wordle.ui.theme.WordleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WordleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WordleTheme {
+        Greeting("Android")
+    }
+}
