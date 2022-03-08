16471552aa57cf86cea849e73686bfa9624cbfa5
Vijay Bhosale <vijay.bhosale@neosoftmail.com>
2022-03-08 14:51:05 +0530

    initial setup done. next i am making ui of 5 words, that will save in respective viewmodel data once entered

--- app/src/main/java/com/xendit/wordle/MainActivity.kt
+++ app/src/main/java/com/xendit/wordle/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.xendit.wordle
 
+import android.icu.text.CaseMap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.xendit.wordle.ui.theme.WordleTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,23 +22,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             WordleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.White)) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        TopAppBar(title = {
+                            Text(text = "Wordle App")
+                        }, modifier = Modifier
+                            .height(60.dp)
+                            .background(color = Color.Blue))
+                    }
+
+
+
                 }
+
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WordleTheme {
-        Greeting("Android")
-    }
-}
+
